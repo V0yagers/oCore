aa8f902f8e89e3177418f90ea735009253f98635
Administrator <1458687266@qq.com>
2021-05-08 14:01:48 +0800

    None

--- src/main/java/rip/oath/oCore/oCore.java
+++ src/main/java/rip/oath/oCore/oCore.java
@@ -1,4 +1,27 @@
 package rip.oath.oCore;
 
-public class oCore {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Discord : Voyagers#4132
+ * Date : 8/5/2021 1:46 CN Timezone
+ */
+
+public final class oCore extends JavaPlugin     {
+
+
+
+
+    @Override
+    public void onEnable() {
+        getLogger().info("oCore has been loaded");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("oCore has been disabled");
+    }
+
 }
