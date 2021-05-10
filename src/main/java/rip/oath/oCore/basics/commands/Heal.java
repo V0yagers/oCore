a122c10338f37d193262a79ba1de2f0f89b1fab9
Administrator <1458687266@qq.com>
2021-05-10 12:50:12 +0800

    Zoot recode / Add heal command

---
+++ src/main/java/rip/oath/oCore/basics/commands/Heal.java
@@ -0,0 +1,27 @@
+package rip.oath.oCore.basics.commands;
+
+import com.qrakn.honcho.command.CommandMeta;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import rip.oath.oCore.utils.CC;
+
+@CommandMeta(label = "heal", permission = "ocore.heal")
+
+public class Heal {
+
+    public void execute(Player player) {
+        player.setHealth(20.0);
+        player.sendMessage(CC.translate(" &aYou have healed by yourself"));
+    }
+
+    public void execute(CommandSender sender, Player player) {
+        if (player == null) {
+            sender.sendMessage(CC.translate("&cPlayer not found"));
+            return;
+        }
+
+        player.setHealth(20.0);
+        player.sendMessage(CC.translate("&cPlayer not found"));
+
+
+    }}
--- src/main/java/rip/oath/oCore/oCore.java
+++ src/main/java/rip/oath/oCore/oCore.java
@@ -1,8 +1,14 @@
 package rip.oath.oCore;
 
+import com.qrakn.honcho.Honcho;
+import lombok.Getter;
+import rip.oath.oCore.basics.commands.Heal;
+import rip.oath.oCore.utils.CC;
+
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 /**
  * Discord : Voyagers#4132
@@ -11,17 +17,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class oCore extends JavaPlugin     {
 
-
-
+    @Getter
+    private Honcho honcho;
 
     @Override
     public void onEnable() {
-        getLogger().info("oCore has been loaded");
+
+        honcho = new Honcho(this);
+
+
+        Arrays.asList(
+                new Heal()
+
+        ).forEach(honcho::registerCommand);
+
+
+
+
+        Bukkit.getConsoleSender().sendMessage(CC.translate(" "));
+        Bukkit.getConsoleSender().sendMessage(CC.translate("&eoCore loaded"));
+        Bukkit.getConsoleSender().sendMessage(CC.translate("&eAuthor : &fOath Development / voyagers258"));
+        Bukkit.getConsoleSender().sendMessage(CC.translate("&eDiscord : &f Voyagers#4132 "));
+        Bukkit.getConsoleSender().sendMessage(CC.translate(" "));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("oCore has been disabled");
+        Bukkit.getConsoleSender().sendMessage(CC.translate("&c&loCore disabled"));
     }
 
 }
